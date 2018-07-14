548f1a08d3e844760dcb192081e91a09df47fde9
Mayank Sinha <mayank.sinha02@gmail.com>
2018-07-14 19:16:02 +0530

    Login/Register

---
+++ app/src/main/java/com/backyardev/instagramcloneproject/HomeActivity.java
@@ -0,0 +1,57 @@
+package com.backyardev.instagramcloneproject;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    ListView listAllUsers;
+    FloatingActionButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_home );
+
+        listAllUsers=findViewById( R.id.listAllUsers );
+
+        FloatingActionButton idLogoutFab = findViewById( R.id.idLogoutFab );
+        FloatingActionButton idCameraFab = findViewById( R.id.idCameraFab );
+        FloatingActionButton idGalleryFab = findViewById( R.id.idGalleryFab );
+
+        idCameraFab.setBackground( getDrawable( R.drawable.camera ) );
+        idLogoutFab.setBackground( getDrawable( R.drawable.logout ) );
+        idGalleryFab.setBackground( getDrawable( R.drawable.gallery ) );
+
+        idLogoutFab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( HomeActivity.this,LoginActivity.class );
+                startActivity( i );
+                finish();
+            }
+        } );
+        idCameraFab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicture = new Intent( MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);
+            }
+        } );
+        idGalleryFab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+            }
+        } );
+    }
+}
--- app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
+++ app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
@@ -1,14 +1,44 @@
 package com.backyardev.instagramcloneproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText editName,editPass;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+        btnLogin=findViewById( R.id.btnLogin );
+        editPass=findViewById( R.id.editPass );
+        editName=findViewById( R.id.editName );
 
+    }
+
+    public void btnLoginAction(View view) {
+
+        boolean login=true;
+        String Name, Pass;
+        Name=editName.getText().toString().trim();
+        Pass=editPass.getText().toString().trim();
+        if (Name.isEmpty()){
+            editName.setError( "Required Field!" );
+            login=false;
+        }
+        if (Pass.isEmpty()){
+            editPass.setError( "Required Field!" );
+            login=false;
+        }
+        if(login){
+            Intent i = new Intent( LoginActivity.this,HomeActivity.class );
+            startActivity( i );
+            finish();
+        }
     }
 }
