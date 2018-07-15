46cdaba494eb277c5c61a45cd3cde01a88510caa
Mayank Sinha <mayank.sinha02@gmail.com>
2018-07-15 16:34:35 +0530

    code cleaned

--- app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
+++ app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
