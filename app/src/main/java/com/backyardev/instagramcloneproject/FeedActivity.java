3172a1d6e19a37449043ab0486f7cd4500807fbd
Mayank Sinha <mayank.sinha02@gmail.com>
2018-07-16 19:53:49 +0530

    posting image

--- app/src/main/java/com/backyardev/instagramcloneproject/FeedActivity.java
+++ app/src/main/java/com/backyardev/instagramcloneproject/FeedActivity.java
@@ -1,17 +1,36 @@
 package com.backyardev.instagramcloneproject;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 public class FeedActivity extends AppCompatActivity {
+
+    ListView listFeed;
+    RelativeLayout progRel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_feed );
-        String name= getIntent().getExtras().getString( "name" );
-        getSupportActionBar().setTitle( Html.fromHtml("<font color=\"black\">" + "Feed: "+name + "</font>"));
+        String name = getIntent().getExtras().getString( "name" );
+        getSupportActionBar().setTitle( Html.fromHtml( "<font color=\"black\">" + "Feed: " + name + "</font>" ) );
+
+        progRel=findViewById( R.id.progRel );
+        progRel.setVisibility( View.VISIBLE );
+        listFeed = findViewById( R.id.listFeed );
+        feedListFeed();
+
+    }
+
+    private void feedListFeed() {
+
+
+
+        progRel.setVisibility( View.INVISIBLE );
     }
 }
--- app/src/main/java/com/backyardev/instagramcloneproject/HomeActivity.java
+++ app/src/main/java/com/backyardev/instagramcloneproject/HomeActivity.java
@@ -1,7 +1,19 @@
 package com.backyardev.instagramcloneproject;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -9,39 +21,70 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE=1;
+    private static final int GALLERY_REQUEST_CODE=2;
+
     ListView listAllUsers;
     FloatingActionButton button;
-    RelativeLayout relProg;
+    RelativeLayout relProg,imageRel;
+    private ProgressDialog progressDialog;
+    private StorageReference storage;
+    FloatingActionsMenu idOpsFab;
+    Button btnUpload,btnCancel;
+    ImageView uploadImageView;
+    String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home );
 
+        progressDialog=new ProgressDialog( this );
         relProg=findViewById( R.id.relProg );
+        imageRel=findViewById( R.id.imageRel );
+        idOpsFab=findViewById( R.id.idOpsFab );
+        btnCancel=findViewById( R.id.btnCancel );
+        btnUpload=findViewById( R.id.btnUpload );
         listAllUsers=findViewById( R.id.listAllUsers );
-        String user=FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        uploadImageView=findViewById( R.id.uploadImageView );
+        user=FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         Toast.makeText( this,"Welcome "+user,Toast.LENGTH_SHORT ).show();
         relProg.setVisibility( View.VISIBLE );
         getSupportActionBar().setTitle( Html.fromHtml("<font color=\"black\">" + getString(R.string.home_title) + "</font>"));
             populateList();
+
+
+            storage=FirebaseStorage.getInstance().getReference();
+        DatabaseReference mDB = FirebaseDatabase.getInstance().getReference().child( "users" );
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
         FloatingActionButton idLogoutFab = findViewById( R.id.idLogoutFab );
         FloatingActionButton idCameraFab = findViewById( R.id.idCameraFab );
@@ -68,18 +111,99 @@ public class HomeActivity extends AppCompatActivity {
         idCameraFab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePicture = new Intent( MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture, 0);
+
+                if (ContextCompat.checkSelfPermission(HomeActivity.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale( HomeActivity.this,
+                            Manifest.permission.CAMERA )) {
+                    } else {
+                        ActivityCompat.requestPermissions( HomeActivity.this,
+                                new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE );
+                    }
+                }
+                Intent intent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE );
+                if (intent.resolveActivity( getPackageManager() ) != null) {
+                    startActivityForResult( intent, CAMERA_REQUEST_CODE );
+                }
             }
         } );
         idGalleryFab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(pickPhoto , 1);
+
+                startActivityForResult(i, GALLERY_REQUEST_CODE);
             }
         } );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            imageRel.setVisibility( View.VISIBLE );
+            idOpsFab.setVisibility( View.INVISIBLE );
+            Bitmap photo = (Bitmap) data.getExtras().get( "data" );
+            uploadImageView.setImageBitmap( photo );
+        }else if(requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            imageRel.setVisibility( View.VISIBLE );
+            idOpsFab.setVisibility( View.INVISIBLE );
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            uploadImageView.setImageBitmap( BitmapFactory.decodeFile(picturePath));
+        }
+        btnCancel.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageRel.setVisibility( View.INVISIBLE );
+                idOpsFab.setVisibility( View.VISIBLE );
+            }
+        } );
+        btnUpload.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.setMessage( "Uploading..." );
+                progressDialog.show();
+                uploadImageView.setDrawingCacheEnabled(true);
+                uploadImageView.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) uploadImageView.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+                String timestamp=String.valueOf( new Date().getTime() );
+                StorageReference storRef =storage.child( user+"'s Photos" ).child( "JPEG_"+timestamp+"_" );
+                UploadTask uploadTask = (UploadTask) storRef.putBytes(data).addOnFailureListener( new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Log.d( "upload","upload failed!" );
+                        Toast.makeText( HomeActivity.this,"Upload Failed!!",Toast.LENGTH_SHORT ).show();
+                    }
+                } ).addOnSuccessListener( new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressDialog.dismiss();
+                        Toast.makeText( HomeActivity.this,"Upload Successful!!",Toast.LENGTH_SHORT ).show();
+                        imageRel.setVisibility( View.INVISIBLE );
+                        idOpsFab.setVisibility( View.VISIBLE );
+                    }
+                } );
+            }
+        } );
+
+
     }
 
     private void populateList() {
@@ -88,7 +212,7 @@ public class HomeActivity extends AppCompatActivity {
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        collectPhoneNumbers((Map<String,Object>) dataSnapshot.getValue());
+                        collectUserNames((Map<String,Object>) dataSnapshot.getValue());
                     }
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
@@ -97,7 +221,7 @@ public class HomeActivity extends AppCompatActivity {
                 });
     }
 
-    private void collectPhoneNumbers(Map<String, Object> name) {
+    private void collectUserNames(Map<String, Object> name) {
         ArrayList<String> userNames = new ArrayList<>();
         for (Map.Entry<String, Object> entry : name.entrySet()){
 
@@ -112,5 +236,4 @@ public class HomeActivity extends AppCompatActivity {
 
         relProg.setVisibility( View.INVISIBLE );
     }
-
 }
