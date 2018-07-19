c6214c62fbda9357fb402f2679f2d2ebd947abfa
Mayank Sinha <mayank.sinha02@gmail.com>
2018-07-19 14:24:54 +0530

    email validation

--- app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
+++ app/src/main/java/com/backyardev/instagramcloneproject/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -60,6 +61,9 @@ public class LoginActivity extends AppCompatActivity {
         }
         if (Pass.isEmpty()){
             editPass.setError( "Required Field!" );
+            login=false;
+        }if( !Patterns.EMAIL_ADDRESS.matcher( Email ).matches()){
+            editEmail.setError( "Invalid Email ID" );
             login=false;
         }
         if(login){
